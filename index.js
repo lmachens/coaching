3b11bea6b84c4d2d1a4b7da30f873a0001c98f00
lmachens <leon.machens@googlemail.com>
2020-10-09 16:18:54 +0200

    Stop logging everything

--- index.js
+++ index.js
@@ -8,11 +8,6 @@ const port = 3000;
 app.use(bodyParser.urlencoded());
 app.use(bodyParser.json());
 
-app.use((req, res, next) => {
-  console.log(req.url, req.method, req.query, req.body);
-  next();
-});
-
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
@@ -21,11 +16,12 @@ app.post("/slack/feedback/modal", async (req, res) => {
   try {
     const payload = JSON.parse(req.body.payload);
     if (payload.type === "view_submission") {
-      console.log(payload.state.values);
-      return res.send();
+      console.log(JSON.stringify(payload, null, 2));
     }
-    await modal.openModal(payload);
-    res.send("Modal opened");
+    if (payload.type === "shortcut") {
+      await modal.openModal(payload);
+    }
+    res.send();
   } catch (error) {
     console.error(error);
     res.status(500).send(error.message);
