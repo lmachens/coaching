231cd05c3c38e6d7c374033200136d5ce8a20b12
lmachens <leon.machens@googlemail.com>
2020-10-09 15:25:49 +0200

    Parse form data

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const modal = require("./slack/feedback/modal");
 const app = express();
 const port = 3000;
 
+app.use(bodyParser.urlencoded());
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {
