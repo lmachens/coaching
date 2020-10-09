d87f29311c7e0dd4b52ee81aabd1bf0c1b3eab85
lmachens <leon.machens@googlemail.com>
2020-10-09 16:55:29 +0200

    Log cells

--- feedback/coda.js
+++ feedback/coda.js
@@ -33,6 +33,7 @@ const sendFeedback = async (feedback) => {
       value,
     };
   });
+  console.log(cells);
   return fetch(
     `https://coda.io/apis/v1/docs/${process.env.CODA_DOC_ID}/tables/${process.env.CODA_TABLE_ID}/rows`,
     {
