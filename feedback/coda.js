7eeb3ca7ac84ffc6e1a53638328c79b499e3bf92
lmachens <leon.machens@googlemail.com>
2020-10-11 17:58:14 +0200

    Remove logs

--- feedback/coda.js
+++ feedback/coda.js
@@ -34,7 +34,6 @@ const sendFeedback = async (feedback) => {
       value: value || "",
     };
   });
-  console.log(cells);
   return fetch(
     `https://coda.io/apis/v1/docs/${process.env.CODA_DOC_ID}/tables/${process.env.CODA_TABLE_ID}/rows`,
     {
