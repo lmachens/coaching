11d8f727a6f90852f86dc7a75775c19e39f4ed66
lmachens <leon.machens@googlemail.com>
2020-10-11 17:34:33 +0200

    Log payload

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ app.post("/slack/feedback/modal", async (req, res) => {
   try {
     const payload = JSON.parse(req.body.payload);
     if (payload.type === "view_submission") {
+      console.log(payload.view.state.values);
       const feedback = Object.values(payload.view.state.values).map((value) => {
         const key = Object.keys(value)[0];
         const item = value[key];
